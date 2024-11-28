a33b829804e2ab2285ea949d66cab9d264ab99a7
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-11-28 05:53:56 +0530

    chore: removed headings
    More of a UX problem, since most users I tested in person with tended to
    click on the separtor rather than the actual section

--- src/renderer/src/components/Settings/Categories.tsx
+++ src/renderer/src/components/Settings/Categories.tsx
@@ -18,14 +18,10 @@ export const Categories = (): React.ReactElement => {
       {/* <h1 className="mt-10 text-4xl">Settings</h1> */}
       <div className="flex flex-col gap-10 lg:flex-row  lg:gap-24">
         <ModelConfiguration className="flex flex-col gap-5">
-          <h1 className="text-xl">Model:</h1>
-          <Separator className="mb-8" />
           <ChooseModel />
           <PullModel />
         </ModelConfiguration>
         <div className="flex flex-col gap-5">
-          <h1 className="text-xl">Theme:</h1>
-          <Separator className="mb-8" />
           <BackgroundSelector />
           <ModeSelector />
         </div>
@@ -37,8 +33,8 @@ export const Categories = (): React.ReactElement => {
   return (
     <>
       <Navbar className='sticky'>
-        <NavbarItem className={`${selected == 'settings' && 'opacity-100'}`} onClick={()=>setSelected('settings')}>Settings</NavbarItem>
-        <NavbarItem className={`${selected == 'knowledgeBase' && 'opacity-100'}`} onClick={()=>setSelected('knowledgeBase')}>Knowledge Base</NavbarItem>
+        <NavbarItem className={`${selected == 'settings' && 'opacity-100'}`} onClick={() => setSelected('settings')}>Settings</NavbarItem>
+        <NavbarItem className={`${selected == 'knowledgeBase' && 'opacity-100'}`} onClick={() => setSelected('knowledgeBase')}>Knowledge Base</NavbarItem>
       </Navbar>
       {map.get(selected)}
     </>
