2f638b1913fe09d3942479823b7265c3dc77e309
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2024-11-28 13:17:42 +0530

    chore: added variants to breadcrumbs component

--- src/renderer/src/ui/BreadCrumb.tsx
+++ src/renderer/src/ui/BreadCrumb.tsx
@@ -1,14 +1,30 @@
 import { cn } from '@renderer/utils/utils'
 import { ComponentProps } from 'react'
 import { Card } from './Card'
+import { VariantProps, cva } from 'class-variance-authority'
 
+
+const BreadCrumbVariants = cva('', {
+  variants: {
+    variant: {
+      base: 'w-fit text-xs p-2 rounded-xl cursor-pointer opacity-50 hover:opacity-100 transition-all',
+      raw: 'w-fit text-xs p-2 rounded-xl cursor-none transition-all'
+    }
+  },
+  defaultVariants: {
+    variant: 'base'
+  }
+})
+
+interface BreadCrumbProps extends ComponentProps<'div'>, VariantProps<typeof BreadCrumbVariants> { }
 export const BreadCrumb = ({
   className,
   children,
+  variant,
   ...props
-}: ComponentProps<'div'>): React.ReactElement => {
+}: BreadCrumbProps): React.ReactElement => {
   return (
-    <Card className={cn('w-fit text-xs p-2 rounded-xl cursor-pointer opacity-50 hover:opacity-100 transition-all', className)} {...props}>
+    <Card className={cn(BreadCrumbVariants({ variant, className }))} {...props}>
       {children}
     </Card>
   )
