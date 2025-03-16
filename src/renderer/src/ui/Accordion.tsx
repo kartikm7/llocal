9052f298300ac7087fd60e7db270d36e537106a9
kartikm7 <108652656+kartikm7@users.noreply.github.com>
2025-03-16 21:32:37 +0530

    refactor: optimized title rerendering
    - decomposed the Accordion into a parent and child component i.e
    Accordion and AccordionTitle
    - used React.memo() to optimize the rerenders

--- src/renderer/src/ui/Accordion.tsx
+++ src/renderer/src/ui/Accordion.tsx
@@ -20,19 +20,33 @@ interface AccordionProps extends ComponentProps<'div'>, VariantProps<typeof Acco
   loading?: boolean,
   initialOpen?: boolean
 }
+
+interface AccordionTitleProps extends ComponentProps<'div'> {
+  title: string,
+  loading: boolean,
+  open: boolean
+}
+
+
+export const AccordionTitle = React.memo(function AccordionTitle({ title, loading, open, className, ...props }: AccordionTitleProps): React.ReactElement {
+
+  console.log("re-render")
+  return <h1 role="button" className={cn("flex justify-between items-center", className)} {...props} >
+    <span className="text-xs flex justify-center items-center gap-2">{loading && <span className="relative flex size-3">
+      <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-slate-400 opacity-75"></span>
+      <span className="relative inline-flex size-3 rounded-full bg-blue-500"></span>
+    </span>}<span className={`${loading && "animation-pulse"}`}>{title}</span> </span>
+    <span className={`transition-all ${open && 'rotate-180'} `}><IoIosArrowDown /></span>
+  </h1>
+}, (preProps, nextProps) => JSON.stringify(preProps) === JSON.stringify(nextProps))
+
 export const Accordion = ({ title, content, loading = false, initialOpen = false, className, variant, ...props }: AccordionProps): React.ReactElement => {
   const [open, setOpen] = useState(initialOpen)
   return <Card
     className={cn(AccordionVariants({ variant, className }), loading && "cursor-default hover:opacity-50")}
     onClick={() => setOpen(pre => !pre)}
     {...props}>
-    <h1 role="button" className="flex justify-between items-center" >
-      <span className="text-xs flex justify-center items-center gap-2">{loading && <span className="relative flex size-3">
-        <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-white-400 opacity-75"></span>
-        <span className="relative inline-flex size-3 rounded-full bg-white-500"></span>
-      </span>}<span className={`${loading && "animation-pulse"}`}>{title}</span> </span>
-      <span className={`transition-all ${open && 'rotate-180'} `}><IoIosArrowDown /></span>
-    </h1>
+    <AccordionTitle title={title} loading={loading} open={open} />
     <div className={`transition-all duration-300 grid overflow-hidden ${open ? 'grid-rows-[1fr] opacity-100' : 'grid-rows-[0fr] opacity-0'}`}>
       <div className="overflow-hidden">
         {content}
