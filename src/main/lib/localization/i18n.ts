8194c4bf80fdeff5142363f856f9b16c4894b24a
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-06-08 17:08:55 +0530

    fix: missing translations
    - this was done, by pointing the loadPath of the i18n config to the
    app.asar.unpacked (man I'm so glad)

--- src/main/lib/localization/i18n.ts
+++ src/main/lib/localization/i18n.ts
@@ -1,18 +1,30 @@
 import i18n from "i18next";
 import backend from "i18next-fs-backend"
 import config from "../../../../resources/i18n.config.json"
+import path from "node:path";
+import { app } from "electron/main";
+import { existsSync } from "node:fs";
 
 export interface i18nConfig {
   preferredLanguage: string
 }
 
 
+/*  TODO: See if we can do this without hardcoding the app.asar.unpacked
+ * */
+let basePath = path.join(app.getAppPath(), "resources/locales")
+
+// this is for production builds where we need to look into the asar.unPack instead
+if (!existsSync(basePath)) {
+  basePath = path.join(app.getAppPath(), "resources/app.asar.unpacked/resources")
+}
+
 i18n
   .use(backend)
   .init({
     backend: {
-      loadPath: "resources/locales/{{lng}}/{{ns}}.json",
-      addPath: "resources/locales/{{lng}}/{{ns}}.missing.json",
+      loadPath: basePath + "/{{lng}}/{{ns}}.json",
+      addPath: basePath + "/{{lng}}/{{ns}}.missing.json",
     },
     // other options you might configure
     debug: false,
