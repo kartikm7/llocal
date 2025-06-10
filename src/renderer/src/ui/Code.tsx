66a8a5c9cae788c7e9b736636d0f2cdfea204b0c
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-06-10 22:12:12 +0530

    style: updated code block style

--- src/renderer/src/ui/Code.tsx
+++ src/renderer/src/ui/Code.tsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react'
 import SyntaxHighlighter, { SyntaxHighlighterProps } from 'react-syntax-highlighter'
-import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/esm/styles/hljs'
-import { toast } from 'sonner'
-import { FaClipboard } from 'react-icons/fa6'
 import { Separator } from './Separator'
 import { HiMiniSparkles } from 'react-icons/hi2'
 import Artifacts from './Artifacts'
-import { t } from '@renderer/utils/utils'
 import { CopyButton } from './CopyButton'
+import { Card } from './Card'
+import { atomOneDarkReasonable } from 'react-syntax-highlighter/dist/cjs/styles/hljs'
 
 // list of supportedArtifacts
 const supportedArtifacts = ['mermaid']
@@ -20,20 +18,20 @@ export const Code = ({
   const [isArtifact, setArtifact] = useState(false)
   return (
     // match[1]
-    <div className='max-w-full max-h-full'>
-      <div className='flex justify-between p-1 items-center rounded-t-md bg-foreground dark:bg-background dark:bg-opacity-75'>
-        <p className='opacity-75'>{language}</p>
+    <Card className='max-w-full max-h-full p-2 my-2'>
+      <div className='flex justify-between p-1 items-center rounded-t-md '>
+        <p className='opacity-50 text-xs'>{language}</p>
         <div className='flex justify-center items-center gap-3'>
           <CopyButton text={children} />
           {supportedArtifacts.includes(language ?? '') && < HiMiniSparkles onClick={() => setArtifact(pre => !pre)} className='cursor-pointer opacity-75 hover:opacity-100' />}
         </div>
       </div>
-      <Separator />
+      <Separator className='h-[1px]' />
       {
-        isArtifact ? <Artifacts code={String(children)} language={language ?? ''} /> : <SyntaxHighlighter {...props} PreTag="div" language={language} style={atomOneDarkReasonable}>
+        isArtifact ? <Artifacts code={String(children)} language={language ?? ''} /> : <SyntaxHighlighter customStyle={{ background: 0 }} PreTag="div" language={language} style={atomOneDarkReasonable} >
           {children}
         </SyntaxHighlighter>
       }
-    </div >
+    </Card >
   )
 }
