9ee177ad37fcb1ab7a21d14e2fa17bd758f04e6b
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-06-19 00:04:47 +0530

    fix: broken width of tooltip

--- src/renderer/src/ui/Card.tsx
+++ src/renderer/src/ui/Card.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-export const Card = ({className, children,...props}:ComponentProps<'div'>):React.ReactElement => {
+export const Card = ({ className, children, ...props }: ComponentProps<'div'>): React.ReactElement => {
   return <div className={twMerge('bg-opacity-20 dark:bg-opacity-20 backdrop-blur-lg bg-foreground dark:bg-background p-5 rounded-2xl max-w-full', className)} {...props}>
     {children}
   </div>
--- src/renderer/src/ui/ToolTip.tsx
+++ src/renderer/src/ui/ToolTip.tsx
@@ -3,7 +3,7 @@ import { BreadCrumb } from "./BreadCrumb";
 import { cn } from "@renderer/utils/utils";
 import { VariantProps, cva } from "class-variance-authority";
 
-const ToolTipVariants = cva('hidden absolute group-hover:block transform z-10', {
+const ToolTipVariants = cva('hidden absolute group-hover/tooltip:block transform z-10', {
   variants: {
     variant: {
       top: 'left-1/2 -translate-x-1/2 bottom-full mb-2',
@@ -20,8 +20,8 @@ const ToolTipVariants = cva('hidden absolute group-hover:block transform z-10', 
 interface ToolTip extends ComponentProps<'div'>, VariantProps<typeof ToolTipVariants> { tooltip: string }
 
 export default function ToolTip({ tooltip, variant, className, children }: ToolTip): React.ReactElement {
-  return <div className={cn('relative group w-fit', className)}>
+  return <div className={cn('relative group/tooltip w-fit', className)}>
     {children}
-    <BreadCrumb className={cn(ToolTipVariants({ variant }))} variant='raw'><p className="w-fit">{tooltip}</p></BreadCrumb>
+    <BreadCrumb className={cn(ToolTipVariants({ variant }), "max-w-none animate-fadeIn")} variant='raw'><p className="w-fit">{tooltip}</p></BreadCrumb>
   </div>
 }
