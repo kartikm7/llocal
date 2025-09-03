9d0d690b9a9a0d011f3270e35a8bf5835726a0d9
Kartikeya Mishra <kartikeyavkm@gmail.com>
2025-09-04 00:57:35 +0530

    fix: increased primary button variant opacity

--- src/renderer/src/ui/Button.tsx
+++ src/renderer/src/ui/Button.tsx
@@ -5,10 +5,10 @@ import { VariantProps, cva } from 'class-variance-authority'
 const ButtonVariants = cva('', {
   variants: {
     variant: {
-      icon: 'hover:scale-105 disabled:transition-none disabled:opacity-50 opacity-60 hover:opacity-100 transition-all p-2',
+      icon: 'hover:scale-105 disabled:transition-none disabled:opacity-50 opacity-60 hover:opacity-100 transition-all',
       link: 'hover:underline hover:scale-[0.98] opacity-60 hover:opacity-100 transition-all',
       breadcrumb: 'w-fit text-xs p-2 rounded-xl cursor-pointer opacity-50 hover:opacity-100 transition-all',
-      primary: 'hover:scale-95 disabled:transition-none disabled:opacity-50 opacity-60 hover:opacity-100 transition-all bg-background bg-opacity-30 hover:opacity-50 p-2 px-3 rounded-xl',
+      primary: 'hover:scale-95 disabled:transition-none disabled:opacity-50 opacity-60 hover:opacity-100 transition-all bg-background bg-opacity-30 p-2 px-3 rounded-xl',
       secondary: 'hover:scale-95 disabled:transition-none disabled:opacity-50 opacity-60 hover:opacity-100 transition-all p-2 px-3 rounded-xl'
     }
   },
